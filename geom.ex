9b2c4628d608b455ff3585411943dc475c0fc342
k-rathi <kunalrathi1994@gmail.com>
2017-07-25 18:50:25 -0700

    finished chapter 3

--- geom.ex
+++ geom.ex
@@ -9,18 +9,20 @@ defmodule Geom do
 	@doc """
 	Calculate the area of a rectangle given width and length.
 	"""	
-	@spec area(atom(), number(), number()) :: number()
-	
-	def area(:rectangle, length, width) when length >= 0 and width >= 0 do
+	@spec area({atom(), number(), number()}) :: number()
+	def area({shape, a, b}) do 
+		area(shape, a, b)
+	end
+	defp area(:rectangle, length, width) when length >= 0 and width >= 0 do
 		length * width
 	end
-	def area(:triangle, base, height) when base >= 0 and height >= 0  do
+	defp area(:triangle, base, height) when base >= 0 and height >= 0  do
 		height * base / 2.0
 	end
-	def area(:ellipse, major, minor) when major >= 0 and minor >= 0 do
+	defp area(:ellipse, major, minor) when major >= 0 and minor >= 0 do
 		:math.pi * major * minor
 	end
-	def area(_, _, _) do
+	defp area(_, _, _) do
 		0
 	end
 
